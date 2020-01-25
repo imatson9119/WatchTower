d7eb15851d3bb15501d7f4b395bead939b9430f1
alexlabbane <alex.labbane@gmail.com>
2020-01-25 14:51:00 -0600

    Pulling from fire and air quality apis

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,10 +11,17 @@ export class HomeComponent implements OnInit {
   constructor(public http : HttpClient) { }
 
   ngOnInit() {
-    let sub = this.http.get("https://api.breezometer.com/air-quality/v2/current-conditions?lat=70&lon=70&key=639074e9fc5b4dd492f3b559390184d3")
+    let sub = this.http.get("https://api.breezometer.com/air-quality/v2/current-conditions?lat=49&lon=2&key=639074e9fc5b4dd492f3b559390184d3")
       .subscribe((value : any) => {
-        console.log(value.data.aqi);
+        console.log(value);
+        console.log(value.data.indexes.baqi.aqi);
       });
+
+    let fireSub = this.http.get("https://api.breezometer.com/fires/v1/current-conditions?lat=-37&lon=148&key=639074e9fc5b4dd492f3b559390184d3&radius=100")
+    .subscribe((value : any) => {
+      console.log(value);
+      console.log(value.data.fires);
+    });
   }
 
 }
