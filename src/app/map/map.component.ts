99ef4ef54124baeb85a04b7b4ca6448e97da5d2e
Scott Steinhauser <scsteinhauser@gmail.com>
2020-01-25 22:05:34 -0600

    google maps

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,7 @@
+
 import { Component, OnInit } from '@angular/core';
+import {} from 'googlemaps';
+import { ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-map',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
+  @ViewChild('map', {static: true}) mapElement: any;
+  map: google.maps.Map;
   constructor() { }
 
   ngOnInit() {
+    const mapProperties = {
+      center: new google.maps.LatLng(35.2271, -80.8431),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.mapElement.nativeElement,    mapProperties);
   }
 
 }
