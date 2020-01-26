f46591af90caf8b02a0a7e101336e82df8cee5bc
alexlabbane <alex.labbane@gmail.com>
2020-01-25 23:08:59 -0600

    Updated environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBlk-cHHTC1h2lxnHxrJnEglbQAkXsdGKU",
+    authDomain: "watchtower-856e5.firebaseapp.com",
+    databaseURL: "https://watchtower-856e5.firebaseio.com",
+    projectId: "watchtower-856e5",
+    storageBucket: "watchtower-856e5.appspot.com",
+    messagingSenderId: "396650392974",
+    appId: "1:396650392974:web:bc638496990be4507148df",
+    measurementId: "G-5TZ6SMLH35"
+  }
 };
 
 /*
