7be94eef19841448f64c631b317bddf3577ff3ae
imatson9119 <imatson9119@gmail.com>
2020-01-25 18:09:41 -0600

    linked air quality data to indicator

--- src/app/air-quality/air-quality.component.ts
+++ src/app/air-quality/air-quality.component.ts
@@ -1,25 +1,32 @@
-import { Component, NgModule } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
-import { single } from './data';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-air-quality',
   templateUrl: './air-quality.component.html',
   styleUrls: ['./air-quality.component.scss']
 })
-export class AirQualityComponent{
+export class AirQualityComponent implements OnInit{
 
   single: any[];
   legend: boolean = true;
   legendPosition: string = 'below';
-
+  ngOnInit(){
+    this.single = [
+      {
+        "name": "Air Quality",
+        "value": this.dataService.airQuality
+      }
+    ];
+  }
   colorScheme = {
     domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
   };
 
-  constructor() {
-    Object.assign(this, { single });
+  constructor(public dataService: DataService) {
+    
   }
 
   onSelect(data): void {
