69f7a7700634c1573934baf53e83ee92a1b1c018
alexlabbane <alex.labbane@gmail.com>
2020-01-26 03:49:09 -0600

    Added coordinates

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -4,6 +4,7 @@ import {} from 'googlemaps';
 import {} from 'google';
 import { ViewChild } from '@angular/core';
 import { DataService } from '../data.service';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -15,12 +16,18 @@ export class MapComponent implements OnInit {
 
   @ViewChild('map', {static: true}) mapElement: any;
   map: google.maps.Map;
-  constructor(public dataService: DataService) {
+  constructor(public dataService: DataService, public http: HttpClient) {
     
   }
 
   ngOnInit() {
     this.map = new google.maps.Map(this.mapElement.nativeElement,    this.dataService.mapProperties);
+    let sub = this.http.get("/").subscribe((values : any) => {
+      let test = "";
+      values.forEach(value => {
+        
+      });
+    });
   }
 
   onRefresh(){
